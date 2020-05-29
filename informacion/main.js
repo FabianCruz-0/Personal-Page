4624ebb6245bf1ebaa9f441dfa36ca5c097f0f96
FabianCruz-0 <fabiancruz3773@gmail.com>
2020-05-29 16:24:12 -0500

    getting ready with the idea

--- informacion/main.js
+++ informacion/main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+var acc = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
